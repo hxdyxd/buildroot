c668cec5baf07c1a5585af913860deda19c26f21
hxdyxd <hxdyxd@gmail.com>
2020-03-20 12:54:48 +0800

    update kernel & add ethoslip_tun

---
+++ package/ethoslip_tun/ethoslip_tun.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# ethoslip_tun
+#
+################################################################################
+
+ETHOSLIP_TUN_VERSION = v0.3
+ETHOSLIP_TUN_SITE = $(call github,hxdyxd,ethoslip_tun,$(ETHOSLIP_TUN_VERSION))
+
+define ETHOSLIP_TUN_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+
+define ETHOSLIP_TUN_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/ethoslip_tun $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/S39ethoslip_tun $(TARGET_DIR)/usr/bin
+endef
+
+
+$(eval $(generic-package))
